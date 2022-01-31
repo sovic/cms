05ef111b6b947b1a88dc7b3d39616e512ddc9f43
Vojtěch Bojda <info@vojtechbojda.cz>
2022-01-31 22:55:01 +0100

    adds pagination class

---
+++ src/Helpers/Pagination.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace SovicCms\Helpers;
+
+use InvalidArgumentException;
+use JetBrains\PhpStorm\Pure;
+
+class Pagination
+{
+    private int $total;
+    private int $perPage;
+    private int $currentPage = 1;
+
+    public function __construct(int $total, int $perPage)
+    {
+        $this->total = $total;
+        $this->perPage = $perPage;
+    }
+
+    #[Pure]
+    public function getPageCount(): int
+    {
+        $pagesCount = $this->getTotal() / $this->getPerPage();
+        if ($pagesCount * $this->getPerPage() < $this->getTotal()) {
+            $pagesCount++;
+        }
+
+        return $pagesCount;
+    }
+
+    public function getPerPage(): int
+    {
+        return $this->perPage;
+    }
+
+    public function setPerPage(int $perPage): void
+    {
+        $this->perPage = $perPage;
+    }
+
+    public function getTotal(): int
+    {
+        return $this->total;
+    }
+
+    public function setTotal(int $total): void
+    {
+        $this->total = $total;
+    }
+
+    public function getCurrentPage(): int
+    {
+        return $this->currentPage;
+    }
+
+    public function setCurrentPage(int $currentPage): void
+    {
+        if ($currentPage < 1) {
+            throw new InvalidArgumentException('invalid page');
+        }
+        $currentPage--; // reset to 0 based
+        if ($currentPage * $this->getPerPage() > $this->getTotal()) {
+            throw new InvalidArgumentException('invalid page');
+        }
+        $this->currentPage = $currentPage;
+    }
+}
