035194fa2b941b78fad4cdc57c189b635079c3ee
Vojtěch Bojda <info@vojtechbojda.cz>
2022-06-11 23:40:40 +0200

    adds post repo tag functions

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -3,7 +3,10 @@
 namespace SovicCms\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
+use SovicCms\Entity\Post;
 use SovicCms\Entity\Tag;
+use SovicCms\Entity\TagPost;
 
 class PostRepository extends EntityRepository
 {
@@ -22,4 +25,36 @@ class PostRepository extends EntityRepository
         return $this->count(['public' => true]);
     }
 
+    public function findPublicByTag(Tag $tag, int $limit = null, int $offset = null): array
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('p')->from(Post::class, 'p');
+        $qb->leftJoin(TagPost::class, 'tp', Join::WITH, 'tp.postId = p.id');
+        $qb->andWhere('p.public = 1');
+        $qb->andWhere('tp.tagId = :tag_id');
+        $qb->setParameter(':tag_id', $tag->getId());
+        $qb->addOrderBy('p.published', 'DESC');
+        $qb->addOrderBy('p.id', 'DESC');
+        if ($limit) {
+            $qb->setMaxResults($limit);
+        }
+        if ($offset) {
+            $qb->setFirstResult($offset);
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function countPublicByTag(Tag $tag): int
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('count(p.id)')->from(Post::class, 'p');
+        $qb->leftJoin(TagPost::class, 'tp', Join::WITH, 'tp.postId = p.id');
+        $qb->andWhere('p.public = 1');
+        $qb->andWhere('tp.tagId = :tag_id');
+        $qb->setParameter(':tag_id', $tag->getId());
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        return $qb->getQuery()->getSingleScalarResult();
+    }
 }
