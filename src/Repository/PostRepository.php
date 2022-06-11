f83ad9010f351151a28376fb014586c1a743a532
Vojtěch Bojda <info@vojtechbojda.cz>
2022-06-11 22:54:32 +0200

    adds PostRepository::findPublic

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -3,8 +3,23 @@
 namespace SovicCms\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use SovicCms\Entity\Tag;
 
 class PostRepository extends EntityRepository
 {
+    public function findPublic(int $limit = null, int $offset = null): array
+    {
+        return $this->findBy(
+            ['public' => true],
+            ['published' => 'DESC', 'id' => 'DESC'],
+            $limit,
+            $offset,
+        );
+    }
+
+    public function countPublic(): int
+    {
+        return $this->count(['public' => true]);
+    }
 
 }
