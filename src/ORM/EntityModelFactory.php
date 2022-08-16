a9903de68bd0e8c20bbf5db6d6e54dbfce92c1f8
Vojtěch Bojda <info@vojtechbojda.cz>
2022-08-16 12:30:46 +0200

    updates EntityModelFactory

--- src/ORM/EntityModelFactory.php
+++ src/ORM/EntityModelFactory.php
@@ -3,27 +3,25 @@
 namespace SovicCms\ORM;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 abstract class EntityModelFactory
 {
     protected EntityManagerInterface $entityManager;
+    protected RouterInterface $router;
+    protected TranslatorInterface $translator;
 
-    public function __construct(EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        RouterInterface        $router,
+        TranslatorInterface    $translator
+    ) {
         $this->entityManager = $entityManager;
+        $this->router = $router;
+        $this->translator = $translator;
     }
 
-    public function getEntityManager(): EntityManagerInterface
-    {
-        return $this->entityManager;
-    }
-
-    /**
-     * @param mixed $entity Entity
-     * @param string $modelClass
-     * @return mixed Model
-     */
     protected function loadEntityModel(mixed $entity, string $modelClass): mixed
     {
         if (null === $entity) {
@@ -32,9 +30,16 @@ abstract class EntityModelFactory
 
         $model = new $modelClass();
         $model->setEntityManager($this->entityManager);
+        $model->setTranslator($this->translator);
+        $model->setRouter($this->router);
         $model->setEntity($entity);
 
         return $model;
+    }
+
+    public function getEntityManager(): EntityManagerInterface
+    {
+        return $this->entityManager;
     }
 
     /**
@@ -48,17 +53,14 @@ abstract class EntityModelFactory
         return $this->loadModelBy($entityClass, $modelClass, ['id' => $id]);
     }
 
-    /**
-     * @param string $entityClass
-     * @param string $modelClass
-     * @param array $criteria
-     * @return mixed
-     */
-    protected function loadModelBy(string $entityClass, string $modelClass, array $criteria): mixed
-    {
-        /** @var EntityRepository $repository */
+    protected function loadModelBy(
+        string $entityClass,
+        string $modelClass,
+        array  $criteria,
+        ?array $orderBy = null
+    ): mixed {
         $repository = $this->entityManager->getRepository($entityClass);
-        $entity = $repository->findOneBy($criteria);
+        $entity = $repository->findOneBy($criteria, $orderBy);
         if (!$entity) {
             return null;
         }
