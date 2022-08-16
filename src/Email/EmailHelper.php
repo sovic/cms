190d609c1bb42b551afe9935845981346db7f309
Vojtěch Bojda <info@vojtechbojda.cz>
2022-08-16 12:38:25 +0200

    adds email manager

---
+++ src/Email/EmailHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SovicCms\Email;
+
+use RuntimeException;
+use Symfony\Component\Mime\Crypto\DkimSigner;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Message;
+
+class EmailHelper
+{
+    public static function signMessage(Email $message, string $domainName = 'mixdrive.sk'): Message
+    {
+        $keyPath = ROOT_PATH . '/dkim.private.key';
+        if (!file_exists($keyPath)) {
+            // no key, no sign
+            throw new RuntimeException('invalid private key');
+        }
+
+        $key = file_get_contents($keyPath);
+
+        return (new DkimSigner($key, $domainName, 'web'))->sign($message);
+    }
+}
---
+++ src/Email/EmailManager.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SovicCms\Email;
+
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class EmailManager
+{
+    private MailerInterface $mailer;
+
+    /**
+     * @param MailerInterface $mailer
+     */
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function send(Email $email): void
+    {
+        // TODO add default email configuration
+//        if (empty($email->getFrom())) {
+//            $email->addFrom('');
+//        }
+//        if (empty($email->getReplyTo())) {
+//            $email->addReplyTo('');
+//        }
+
+        // $message = EmailHelper::signMessage($email); // not working for now
+
+        $this->mailer->send($email);
+    }
+}
