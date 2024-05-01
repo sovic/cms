51fce59eae9b87d2bf94ea2e5222e41c86a8ec30
Vojtěch Bojda <info@vojtechbojda.cz>
2024-05-02 00:27:14 +0200

    adds ProjectFactory::loadByRequest

--- src/Project/ProjectFactory.php
+++ src/Project/ProjectFactory.php
@@ -4,11 +4,34 @@ namespace Sovic\Cms\Project;
 
 use Sovic\Cms\ORM\EntityModelFactory;
 use Sovic\Cms\Entity\Project as ProjectEntity;
+use Symfony\Component\HttpFoundation\Request;
 
 final class ProjectFactory extends EntityModelFactory
 {
     public function loadById(int $id): ?Project
     {
         return $this->loadModelBy(ProjectEntity::class, Project::class, ['id' => $id]);
+    }
+
+    public function loadByRequest(Request $request): ?Project
+    {
+        $envProject = $request->server->get('PROJECT');
+        if ($envProject) {
+            return $this->loadModelBy(ProjectEntity::class, Project::class, ['slug' => $envProject]);
+        }
+
+        $host = $request->getHost();
+        $em = $this->getEntityManager();
+        $result = $em->getRepository(ProjectEntity::class)
+            ->createQueryBuilder('p')
+            ->where('p.domains LIKE :host')
+            ->setParameter('host', '%' . $host . '%')
+            ->getQuery()
+            ->getResult();
+        if ($result) {
+            return $this->loadEntityModel($result[0], Project::class);
+        }
+
+        return null;
     }
 }
