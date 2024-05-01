aaafb4e003ecb16cdf4012a75f5f7c700064876d
Vojtěch Bojda <info@vojtechbojda.cz>
2024-05-01 15:22:28 +0200

    adds post detail controller method

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -7,8 +7,9 @@ use RuntimeException;
 use Sovic\Cms\Post\PostFactory;
 use Sovic\Cms\Post\PostResultSetFactory;
 use Sovic\Cms\Project\ProjectFactory;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Twig\Environment;
 
 class PostsController extends BaseController
@@ -51,7 +52,8 @@ class PostsController extends BaseController
     #[Route('/posts/{pageNr}', name: 'blog_index', requirements: ['pageNr' => '\d+'], defaults: ['pageNr' => 1])]
     public function index(int $pageNr, Environment $twig, ?string $tagName = null): Response
     {
-        $settings = $this->project->getSettings();
+        $project = $this->project;
+        $settings = $project->getSettings();
         $perPage = $settings->get('posts.per_page') ?? 9;
 
         if ($tagName) {
@@ -69,7 +71,8 @@ class PostsController extends BaseController
             $template = $projectTemplate;
         }
 
-        $this->assignArray($this->project->getSettings()->getTemplateData());
+        $this->assign('project', $project->entity->getSlug());
+        $this->assignArray($settings->getTemplateData());
 
         return $this->render($template);
     }
@@ -81,11 +84,57 @@ class PostsController extends BaseController
         return $this->index($pageNr, $twig, $tagName);
     }
 
+    #[Route('/stories/{urlId}', name: 'stories_detail', defaults: [])]
     #[Route('/posts/{urlId}', name: 'posts_detail', defaults: [])]
-    public function post(string $urlId): Response
+    public function post(string $urlId, Environment $twig, Request $request): Response
     {
         $response = $this->loadPost($urlId);
+        if ($response !== null) {
+            return $response;
+        }
+        $post = $this->getPost();
+        if (null === $post) {
+            return $this->show404();
+        }
 
-        return $response ?? $this->render('post/show.html.twig');
+        $secret = $request->get('secret');
+        $isAuthorized = !empty($secret) && $secret === $post->entity->getSecret();
+        if (!$isAuthorized && !$post->entity->isPublic()) {
+            return $this->show404();
+        }
+
+        $downloads = [];
+        if ($isAuthorized) {
+            $gallery = $post->getGalleryManager()->getGallery('downloads');
+            if ($gallery) {
+                $downloads = $gallery->getItems();
+            }
+        }
+
+        $project = $this->project;
+        $settings = $project->getSettings();
+
+        $template = 'post/detail.html.twig';
+        $projectTemplate = 'projects/' . $this->project->entity->getSlug() . '/post/detail.html.twig';
+        if ($twig->getLoader()->exists($projectTemplate)) {
+            $template = $projectTemplate;
+        }
+
+        $cover = $post->getGalleryManager()->getGallery('post')?->getCoverImage();
+        if ($cover) {
+            // TODO add gallery->setBaseUrl() method
+            $baseUrl = $settings->get('gallery.base_url');
+            $cover['small'] = $baseUrl . '/' . $cover['small'];
+            $cover['big'] = $baseUrl . '/' . $cover['big'];
+            $cover['full'] = $baseUrl . '/' . $cover['full'];
+        }
+
+        $this->assign('cover', $cover);
+        $this->assign('downloads', $downloads);
+        $this->assign('has_parallax', $cover !== null);
+        $this->assign('project', $project->entity->getSlug());
+        $this->assignArray($settings->getTemplateData());
+
+        return $this->render($template);
     }
 }
