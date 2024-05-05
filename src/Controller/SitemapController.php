09f72707ae693772a485cd155c454c9012e06991
Vojtěch Bojda <info@vojtechbojda.cz>
2024-05-05 17:09:36 +0200

    updates annotations to attributes

--- src/Controller/SitemapController.php
+++ src/Controller/SitemapController.php
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\UrlHelper;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 abstract class SitemapController extends AbstractController
 {
