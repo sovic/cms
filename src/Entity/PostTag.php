9c9feed963659e8f16da711b572b46e2c3578c29
Vojtěch Bojda <info@vojtechbojda.cz>
2024-05-30 00:49:44 +0200

    updates PostTag relation entity

--- src/Entity/PostTag.php
+++ src/Entity/PostTag.php
@@ -6,28 +6,31 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Index;
 use Doctrine\ORM\Mapping\JoinColumn;
 use Doctrine\ORM\Mapping\ManyToOne;
 use Doctrine\ORM\Mapping\Table;
 
 #[Table(name: 'post_tag')]
+#[Index(columns: ['post_id'], name: 'post_id')]
+#[Index(columns: ['tag_id'], name: 'tag_id')]
 #[Entity]
 class PostTag
 {
-    #[Column(name: 'posts_id', type: Types::INTEGER)]
+    #[Column(name: 'post_id', type: Types::INTEGER)]
     #[Id]
     protected int $postId;
 
     #[ManyToOne(targetEntity: Post::class)]
-    #[JoinColumn(name: 'posts_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    #[JoinColumn(name: 'post_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
     protected Post $post;
 
-    #[Column(name: 'tags_id', type: Types::INTEGER)]
+    #[Column(name: 'tag_id', type: Types::INTEGER)]
     #[Id]
     protected int $tagId;
 
     #[ManyToOne(targetEntity: Tag::class)]
-    #[JoinColumn(name: 'tags_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    #[JoinColumn(name: 'tag_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
     protected Tag $tag;
 
     public function getPostId(): int
