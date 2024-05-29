5d504ae61059486da68301f6fd046cd6e0a9e690
Vojtěch Bojda <info@vojtechbojda.cz>
2024-05-29 20:31:29 +0200

    adds bundle file

---
+++ src/CmsBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sovic\Cms;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CmsBundle extends Bundle
+{
+    public function registerCommands(Application $application): void
+    {
+
+    }
+}
