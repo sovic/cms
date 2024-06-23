36c42c0cdc32868228ad1f6eca37e6b610e398ca
Vojtěch Bojda <info@vojtechbojda.cz>
2024-06-23 13:55:26 +0200

    adds set tag private task

---
+++ src/Command/SetTagPrivateCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Sovic\Cms\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Sovic\Cms\Command\Trait\TagCommandTrait;
+use Sovic\Cms\Project\ProjectFactory;
+use Sovic\Cms\Tag\TagFactory;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+#[AsCommand(name: 'tag:set-private', description: 'Set tag private + slug')]
+class SetTagPrivateCommand extends Command
+{
+    use TagCommandTrait;
+
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+        private readonly ProjectFactory         $projectFactory,
+        private readonly RouterInterface        $router,
+        private readonly TagFactory             $tagFactory,
+
+    ) {
+        parent::__construct();
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $tag = $this->loadTagById($input, $output);
+        if (!$tag) {
+            return Command::FAILURE;
+        }
+
+        $slug = $tag->generateUniqueSlug(32);
+        $tag->entity->setPrivateSlug($slug);
+        $tag->entity->setIsPublic(0);
+        $tag->flush();
+
+        $project = $this->projectFactory->loadByEntity($tag->entity->getProject());
+        $domains = $project->entity->getDomains();
+        $mainDomain = explode("\n", $domains)[0] ?? null;
+
+        $url = $this->router->generate('posts_tag', ['tagName' => $slug]);
+        if ($mainDomain) {
+            $url = $mainDomain . $url;
+        }
+
+        $output->writeln('Tag private url: ' . $url);
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Command/Trait/TagCommandTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Sovic\Cms\Command\Trait;
+
+use Sovic\Cms\Tag\Tag;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+
+trait TagCommandTrait
+{
+    private function loadTagById(InputInterface $input, OutputInterface $output): ?Tag
+    {
+        $helper = $this->getHelper('question');
+        $question = new Question('Tag ID: ');
+        $id = $helper->ask($input, $output, $question);
+        if (empty($id)) {
+            return null;
+        }
+        $tag = $this->tagFactory->loadById($id);
+        if (!$tag) {
+            $output->writeln('Post not found with ID: ' . $id);
+
+            return null;
+        }
+
+        return $tag;
+    }
+}
