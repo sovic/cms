650806aab5af5525633ce2ecb0b2c5b031753608
Vojtěch Bojda <info@vojtechbojda.cz>
2025-02-02 12:22:29 +0100

    fixes GalleryController

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class GalleryController extends BaseController
+class GalleryController extends BaseController implements ProjectControllerInterface
 {
     use DownloadTrait;
     use PostsControllerTrait;
