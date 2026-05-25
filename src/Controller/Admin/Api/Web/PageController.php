98369ad4020c9251c7d13c0f64ca71ee15f5d3ee
Vojtěch Bojda <info@vojtechbojda.cz>
2026-05-25 16:36:33 +0200

    enhance list.html.twig and add PageController.php to implement toggle functionality for page visibility

---
+++ src/Controller/Admin/Api/Web/PageController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Sovic\Cms\Controller\Admin\Api\Web;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Sovic\Cms\Controller\Admin\Api\AbstractBaseApiController;
+use Sovic\Cms\Entity\Page;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PageController extends AbstractBaseApiController
+{
+    private const ToggleableFields = [
+        'is_public',
+    ];
+
+    #[Route(
+        '/admin/api/web/page/{id}/toggle-state',
+        name: 'admin:api:web:page:toggle-state',
+        requirements: ['id' => '\d+'],
+        methods: ['POST'],
+    )]
+    public function toggleState(
+        int                    $id,
+        EntityManagerInterface $em,
+        Request                $request,
+    ): JsonResponse {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $page = $em->getRepository(Page::class)->find($id);
+        if ($page === null) {
+            return $this->sendFail(404);
+        }
+
+        $data = $this->getRequestData($request);
+        $field = $data['field'] ?? null;
+        if (!in_array($field, self::ToggleableFields, true)) {
+            $this->addError('invalid_field');
+
+            return $this->sendFail();
+        }
+
+        $state = $data['state'] ?? null;
+
+        if ($field === 'is_public') {
+            $page->setIsPublic($state);
+            $em->flush();
+
+            $this->data['value'] = $page->isPublic();
+
+            return $this->sendSuccess();
+        }
+
+        return $this->sendFail();
+    }
+}
